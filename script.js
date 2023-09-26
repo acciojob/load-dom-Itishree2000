abdb7639eb0644ceaa0cf58a7157904cf5e4c1d0
Itishree2000 <127124225+Itishree2000@users.noreply.github.com>
2023-09-26 19:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 // Function to handle DOM load event
 function handleDomLoad() {
     // Create a new paragraph element
@@ -8,7 +7,7 @@ function handleDomLoad() {
     paragraph.textContent = "DOM load success";
 
     // Append the paragraph to the body of the document
-    document.body.appendChild(paragraph);
+     document.body.appendChild(paragraph);
 }
 
 // Add an event listener to execute the handleDomLoad function when the DOM is loaded
